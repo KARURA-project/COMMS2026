6746037ab2549c768257cc05b2efc04b8c480665
karura <karura@karura.myguest.virtualbox.org>
2025-07-16 00:34:45 -0500

    update with racs2 repo

---
+++ racs2_demo/ros2_ws/build/bridge_py_s/share/bridge_py_s/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/karura/racs2_ws/ros2_ws/build/bridge_py_s"
---
+++ racs2_demo/ros2_ws/build/bridge_py_s/share/bridge_py_s/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/home/karura/racs2_ws/ros2_ws/build/bridge_py_s"
